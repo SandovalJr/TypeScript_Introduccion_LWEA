3520c799a95608090826f14b927a96b7693f0487
Eduardo Sandoval <edubizzle7@gmail.com>
2020-10-17 10:06:47 -0500

    consolas y numeros de introduccion

---
+++ Introduccion/1_proyecto/app.js
@@ -0,0 +1,9 @@
+// CONSOLAS
+console.log("Bienvenidos");
+console.warn("AVISO IMPORTANTE");
+console.error("ERROR");
+console.clear();
+// NUMEROS
+var edad = 13;
+var puntos = 15;
+console.log(edad + puntos);
---
+++ Introduccion/1_proyecto/app.ts
@@ -0,0 +1,12 @@
+// CONSOLAS
+console.log("Bienvenidos");
+console.warn("AVISO IMPORTANTE");
+console.error("ERROR");
+
+console.clear();
+
+// NUMEROS
+var edad: number = 13;
+var puntos: number = 15;
+
+console.log(edad + puntos);
