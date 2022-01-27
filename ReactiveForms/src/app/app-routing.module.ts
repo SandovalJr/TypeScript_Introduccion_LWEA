e8b95e1819b67be5dbc02a2b7699b0715cd0979e
Eduardo Sandoval <edubizzle7@gmail.com>
2022-01-26 19:56:25 -0600

    formulario

--- ReactiveForms/src/app/app-routing.module.ts
+++ ReactiveForms/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FormularioComponent } from '../app/components/formulario/formulario.component';
+
+const routes: Routes = [{ path: '', component: FormularioComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
