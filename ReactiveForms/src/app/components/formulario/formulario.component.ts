20a3529383dc6d05e15b8e4d9edb073f3dfebe8e
Eduardo Sandoval <edubizzle7@gmail.com>
2022-01-31 17:07:50 -0600

    si agrega correctamente todo

--- ReactiveForms/src/app/components/formulario/formulario.component.ts
+++ ReactiveForms/src/app/components/formulario/formulario.component.ts
@@ -6,6 +6,12 @@ import {
   FormGroup,
 } from '@angular/forms';
 
+interface usuario {
+  nombre: string;
+  email: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
@@ -13,22 +19,25 @@ import {
 })
 export class FormularioComponent implements OnInit {
   formularioCreado: FormGroup;
+  usuarios: Array<usuario> = new Array<usuario>();
 
   constructor(private fb: FormBuilder) {
     this.formularioCreado = this.fb.group({
       nombre: ['Carmen', Validators.required],
       email: ['', Validators.compose([Validators.required, Validators.email])],
       password: [
-        '',
+        'password',
         Validators.compose([Validators.required, Validators.minLength(8)]),
       ],
     });
-
   }
 
   ngOnInit(): void {}
 
   agregar() {
     console.log(this.formularioCreado.value);
+    this.usuarios.push(this.formularioCreado.value as usuario);
+    //borrar los campos anteriores para que el nuevo usuario se pueda registrar
+    this.formularioCreado.reset();
   }
 }
