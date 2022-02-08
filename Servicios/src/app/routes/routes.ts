9d9d9b08391cbe33dd932737b139a051a7d531b0
SandovalJr <edubizzle7@gmail.com>
2022-02-07 22:35:12 -0600

    holis

---
+++ Servicios/src/app/routes/routes.ts
@@ -0,0 +1,9 @@
+import { RouterModule, Routes } from '@angular/router';
+import { Component } from "@angular/core";
+
+
+const routes: Routes = [
+    
+]
+
+export const APP_ROUTES = RouterModule.forRoot(routes);
